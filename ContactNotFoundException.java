6c4c71b449f70ca6cfe1b627b79f9b68c4bf8465
Priya <priya_kumari1@persistent.com>
2021-03-07 17:23:21 +0530

    Assignment 10

---
+++ ContactNotFoundException.java
@@ -0,0 +1,8 @@
+package assignment10;
+
+public class ContactNotFoundException extends Exception{
+	public ContactNotFoundException(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println(string);
+	}
+}
